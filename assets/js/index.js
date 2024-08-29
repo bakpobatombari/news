6b3c8b3ff62af68acdbd85bd7f3a2e3fed2983c6
bakpobatombari <122733659+bakpobatombari@users.noreply.github.com>
2024-08-29 23:10:17 +0100

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+let sidenav = document.getElementById('sidenav');
+
+function openNav(){
+    sidenav.style.width = "50% !important";
+    sidenav.style.display = 'block';
+}
+function closeNav(){
+    sidenav.style.display = 'none';
+}
